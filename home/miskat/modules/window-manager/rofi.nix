0eec45d81ed4544b1417b95d1e56049dc019a05a
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2025-10-28 16:28:35 +0600

    On branch main Changes to be committed: 	modified:   home/miskat/modules/window-manager/hyprland.nix 	modified:   home/miskat/modules/window-manager/rofi.nix 	modified:   home/miskat/modules/window-manager/waybar.nix

--- home/miskat/modules/window-manager/rofi.nix
+++ home/miskat/modules/window-manager/rofi.nix
@@ -9,10 +9,4 @@
       drun-show-actions = true;
     };
   };
-
-  # Make sure code and zen-browser are available for launchers
-  home.packages = with pkgs; [
-    vscode
-    zen-browser
-  ];
 }
--- home/miskat/modules/window-manager/waybar.nix
+++ home/miskat/modules/window-manager/waybar.nix
@@ -33,6 +33,5 @@
     grimblast
     brightnessctl
     hyprlock
-    zen-browser
   ];
 }
