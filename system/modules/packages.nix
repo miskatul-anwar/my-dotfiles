51922051e0c03bc363524fb3fd29cefa919dc7a4
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2025-10-28 10:39:54 +0600

    FIXED: lsd command missing

--- system/modules/packages.nix
+++ system/modules/packages.nix
@@ -1,5 +1,5 @@
 {config, pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    vim lunarvim wget curl google-chrome zsh-powerlevel10k ghostty pciutils zoom-us libgccjit rustup 
+    vim lunarvim wget curl google-chrome zsh-powerlevel10k ghostty pciutils zoom-us libgccjit rustup lsd
   ];
 }
