5d3ab0988b5c8b1b974c849af21431eec2bcfd6d
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2025-10-29 16:42:17 +0600

    On branch main Changes to be committed: 	modified:   home/miskat/modules/files.nix 	modified:   system/modules/codium.nix
    Applied customizations via home.nix and codium.nix in VScodium

--- home/miskat/modules/files.nix
+++ home/miskat/modules/files.nix
@@ -1,4 +1,36 @@
 { ... }: { 
   home.file = {
+    ".config/VSCodium/User/keybindings.json".text = ''
+      [
+        {
+          "key": "tab",
+          "command": "selectNextSuggestion",
+          "when": "suggestWidgetVisible"
+        },
+        {
+          "key": "shift+tab",
+          "command": "selectPrevSuggestion",
+          "when": "suggestWidgetVisible"
+        }
+      ]
+    '';
+ 
+    ".config/VSCodium/User/settings.json".text = ''
+      {
+        "workbench.colorTheme": "GitHub Dark",
+        "window.commandCenter": false,
+        "workbench.layoutControl.enabled": false,
+        "editor.fontFamily": "JetBrains Mono",
+        "editor.fontLigatures": true,
+
+        "code-runner.executorMap": {
+          "c": "cd $dir && nix-shell -p gcc glibc.dev glibc binutils --run 'gcc \"$file\" -o \"$fileNameWithoutExt\" && ./\"$fileNameWithoutExt\"'",
+          "cpp": "cd $dir && nix-shell -p gcc glibc.dev glibc binutils --run 'g++ \"$file\" -o \"$fileNameWithoutExt\" && ./\"$fileNameWithoutExt\"'"
+        },
+
+        "code-runner.runInTerminal": true,
+        "code-runner.saveFileBeforeRun": true
+      }
+    '';
   };
 }
--- system/modules/codium.nix
+++ system/modules/codium.nix
@@ -3,7 +3,15 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      jnoortheen.nix-ide
       llvm-vs-code-extensions.vscode-clangd
+      github.github-vscode-theme
+      rust-lang.rust-analyzer
+      pylyzer.pylyzer
+      usernamehw.errorlens
+      christian-kohler.path-intellisense
+      formulahendry.code-runner
     ];
   };
 }
